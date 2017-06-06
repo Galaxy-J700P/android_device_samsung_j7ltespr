82c31d8fc632b651c2a3c0a2d9fbc07be7a58f0d
minz1 <emerytang@gmail.com>
2017-06-06 15:51:21 -0400

    j7ltespr: add mixer_paths.xml

--- device.mk
+++ device.mk
@@ -47,7 +47,8 @@ PRODUCT_COPY_FILES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Media
 PRODUCT_COPY_FILES += \

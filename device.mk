4764532ffedc69e72aeb479de646221f5ce4b8e6
minz1 <emerytang@gmail.com>
2017-06-06 15:20:30 -0400

    j7ltespr: import keylayout

--- device.mk
+++ device.mk
@@ -17,6 +17,13 @@
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/j7ltespr/j7ltespr-vendor.mk)
 
+# Prebuilt kl keymaps
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
+    $(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
